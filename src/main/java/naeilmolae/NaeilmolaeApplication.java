b156240c62d268478401acebdcbda8ab73e66452
shimfff <shimseawon1510@gmail.com>
2024-11-18 21:15:42 +0900

    init

---
+++ src/main/java/naeilmolae/NaeilmolaeApplication.java
@@ -0,0 +1,13 @@
+package naeilmolae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaeilmolaeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaeilmolaeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/naeilmolae/NaeilmolaeApplicationTests.java
@@ -0,0 +1,13 @@
+package naeilmolae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaeilmolaeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
