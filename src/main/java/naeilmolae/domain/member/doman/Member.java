5158f4543f64e2f73886ff9bc8aad42f48729afa
shimfff <shimseawon1510@gmail.com>
2024-11-19 15:02:52 +0900

    :sparkles: feat: 임시 member 엔티티와 BaseEntity 추가 #5

---
+++ src/main/java/naeilmolae/domain/member/doman/Member.java
@@ -0,0 +1,25 @@
+package naeilmolae.domain.member.doman;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import naeilmolae.global.common.base.BaseEntity;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Setter
+    private String name;
+
+}
