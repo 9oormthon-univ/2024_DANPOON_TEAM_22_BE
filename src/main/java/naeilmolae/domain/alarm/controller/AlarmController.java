943c80ba23a91a81564c446aaaa318c25a96c307
YUSEONG NOH <118250509+USEONGEE@users.noreply.github.com>
2024-11-23 17:25:53 +0900

    ✨ feat: [봉사자] 녹음 개발 #8

--- src/main/java/naeilmolae/domain/alarm/controller/AlarmController.java
+++ src/main/java/naeilmolae/domain/alarm/controller/AlarmController.java
@@ -1,8 +1,12 @@
 package naeilmolae.domain.alarm.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import naeilmolae.domain.alarm.domain.Alarm;
+import naeilmolae.domain.alarm.domain.CategoryType;
+import naeilmolae.domain.alarm.dto.AlarmCategoryCount;
 import naeilmolae.domain.alarm.dto.response.AlarmCategoryResponseDtoWithChildren;
 import naeilmolae.domain.alarm.dto.response.AlarmResponseDto;
 import naeilmolae.domain.alarm.service.AlarmService;
@@ -47,4 +51,28 @@ public class AlarmController {
         return BaseResponse.onSuccess(alarmResponseDto);
     }
 
+    @Operation(summary = "[VALID] [봉사자] 녹음 1단계: 녹음 선택 리스트 조회", description = "사용자의 녹음할 리스트를 조회합니다. 정렬은 1. 사용자가 작성하지 않음 2. 데이터 수가 적음 순으로 보여지게 됩니다. ")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "COMMON200", description = "조회 성공"),
+    })
+    @GetMapping("/list/{categoryType}")
+    public BaseResponse<List<AlarmCategoryCount>> getAlarmList(@CurrentMember Member member,
+                                                               @PathVariable CategoryType categoryType) {
+        List<AlarmCategoryCount> collect = alarmViewService.findUserCategoryCount(member.getId(), categoryType);
+        return BaseResponse.onSuccess(collect);
+    }
+
+    @Operation(summary = "[VALID] [봉사자] 녹음 2단계: 말 작성 시 상단 멘트 조회", description = "위로 녹음9 에서 상단에 보여줄 멘트를 제공합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "COMMON200", description = "조회 성공"),
+    })
+    @GetMapping("/alarm-category/{alarmCategoryId}/optimized")
+    public BaseResponse<AlarmResponseDto> getAlarmCategory(@CurrentMember Member member,
+                                                           @PathVariable Long alarmCategoryId) {
+        Alarm recommendedAlarm = alarmViewService.findRecommendedAlarm(member.getId(), alarmCategoryId);
+        String title = recommendedAlarm.getAlarmCategory().getTitle();
+        Long id = recommendedAlarm.getId();
+        AlarmResponseDto alarmResponseDto = new AlarmResponseDto(id, title);
+        return BaseResponse.onSuccess(alarmResponseDto);
+    }
 }
