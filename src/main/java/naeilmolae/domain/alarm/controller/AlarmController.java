d58721bcd6423b7bcf0d182c15a3be68bb3a1ebe
YUSEONG NOH <118250509+USEONGEE@users.noreply.github.com>
2024-11-23 17:24:35 +0900

    ✨ feat: [청년] 위로 청취 1단계 #8

---
+++ src/main/java/naeilmolae/domain/alarm/controller/AlarmController.java
@@ -0,0 +1,38 @@
+package naeilmolae.domain.alarm.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+import naeilmolae.domain.alarm.dto.response.AlarmCategoryResponseDtoWithChildren;
+import naeilmolae.domain.alarm.service.AlarmService;
+import naeilmolae.domain.alarm.service.AlarmViewService;
+import naeilmolae.domain.member.domain.Member;
+import naeilmolae.global.common.base.BaseResponse;
+import naeilmolae.global.config.security.auth.CurrentMember;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/alarm")
+public class AlarmController {
+    private final AlarmViewService alarmViewService;
+    private final AlarmService alarmService;
+
+
+    // valid
+    @Operation(summary = "[VALID] [청년] 위로 청취 1단계: 위로 목록 조회", description = "위로 목록을 조회합니다. 이후 '[청년] 청취 1단계'로 이동합니다. ")
+    @GetMapping("/alarm-category/comfort")
+    public BaseResponse<List<AlarmCategoryResponseDtoWithChildren>> getComfortList(@CurrentMember Member member) {
+        List<AlarmCategoryResponseDtoWithChildren> collect = alarmViewService.findComfortAlarmCategoryList()
+                .stream()
+                .map(AlarmCategoryResponseDtoWithChildren::new)
+                .collect(Collectors.toList());
+        return BaseResponse.onSuccess(collect);
+    }
+
+
+}
