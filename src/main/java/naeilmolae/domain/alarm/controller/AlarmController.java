077415b248bc31238acc701cab566f2a0c305cef
YUSEONG NOH <118250509+USEONGEE@users.noreply.github.com>
2024-11-23 17:25:17 +0900

    ✨ feat: [청년] 위로 청취 2단계

--- src/main/java/naeilmolae/domain/alarm/controller/AlarmController.java
+++ src/main/java/naeilmolae/domain/alarm/controller/AlarmController.java
@@ -2,13 +2,16 @@ package naeilmolae.domain.alarm.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
+import naeilmolae.domain.alarm.domain.Alarm;
 import naeilmolae.domain.alarm.dto.response.AlarmCategoryResponseDtoWithChildren;
+import naeilmolae.domain.alarm.dto.response.AlarmResponseDto;
 import naeilmolae.domain.alarm.service.AlarmService;
 import naeilmolae.domain.alarm.service.AlarmViewService;
 import naeilmolae.domain.member.domain.Member;
 import naeilmolae.global.common.base.BaseResponse;
 import naeilmolae.global.config.security.auth.CurrentMember;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -34,5 +37,14 @@ public class AlarmController {
         return BaseResponse.onSuccess(collect);
     }
 
+    // valid
+    @Operation(summary = "[VALID] [청년] 위로 청취 2단계: 1단계로 AlarmId 조회", description = "위로 목록을 조회합니다. 이후 '[청년] 청취 1단계'로 이동합니다. ")
+    @GetMapping("/alarm-category/{alarmCategoryId}")
+    public BaseResponse<AlarmResponseDto> getAlarmIdByAlarmCategoryId(@CurrentMember Member member,
+                                                                      @PathVariable Long alarmCategoryId) {
+        Alarm alarm = alarmService.findByAlarmCategoryId(alarmCategoryId);
+        AlarmResponseDto alarmResponseDto = new AlarmResponseDto(alarm.getId(), alarm.getAlarmCategory().getTitle());
+        return BaseResponse.onSuccess(alarmResponseDto);
+    }
 
 }
