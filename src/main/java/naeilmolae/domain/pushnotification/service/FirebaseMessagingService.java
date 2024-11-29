e27a64f2280f590bf6d2962e300c3ec47d5d5c8e
shimfff <shimseawon1510@gmail.com>
2024-11-29 15:57:00 +0900

    :recycle: refactor: logger 추가
    #63

--- src/main/java/naeilmolae/domain/pushnotification/service/FirebaseMessagingService.java
+++ src/main/java/naeilmolae/domain/pushnotification/service/FirebaseMessagingService.java
@@ -3,27 +3,33 @@ package naeilmolae.domain.pushnotification.service;
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.Message;
 import com.google.firebase.messaging.Notification;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 public class FirebaseMessagingService {
 
-    public void sendNotification(String fcmToken, String title, Long fileId) {
+    private static final Logger logger = LoggerFactory.getLogger(FirebaseMessagingService.class);
+
+    public void sendNotification(String fcmToken, String title, Long alarmId) {
         // FirebaseMessage 생성
         Message message = Message.builder()
                 .setToken(fcmToken)
                 .setNotification(Notification.builder()
                         .setTitle(title)
                         .build())
-                .putData("alarmId", String.valueOf(fileId)) // 파일 ID를 데이터로 추가
+                .putData("alarmId", String.valueOf(alarmId)) // 파일 ID를 데이터로 추가
                 .build();
 
         try {
             // FCM에 메시지 전송
             String response = FirebaseMessaging.getInstance().send(message);
-            System.out.println("Successfully sent message: " + response);
+            logger.info("Successfully sent message: {}", response);
+            logger.info("Notification details - FCM Token: {}, Title: {}, Alarm ID: {}", fcmToken, title, alarmId);
         } catch (Exception e) {
-            System.err.println("Error sending message: " + e.getMessage());
+            logger.error("Error sending message: {}", e.getMessage());
+            logger.error("Failed to send notification to FCM Token: {}, Title: {}, Alarm ID: {}", fcmToken, title, alarmId, e);
         }
     }
 }
