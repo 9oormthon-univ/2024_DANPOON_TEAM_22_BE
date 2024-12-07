6bee32100738cd4e476093774a0337184631207e
shimfff <shimseawon1510@gmail.com>
2024-12-07 16:40:47 +0900

    :memo: docs: 주석 삭제

--- src/main/java/naeilmolae/domain/pushnotification/service/PushNotificationService.java
+++ src/main/java/naeilmolae/domain/pushnotification/service/PushNotificationService.java
@@ -13,8 +13,6 @@ import org.springframework.transaction.annotation.Transactional;
 import java.time.LocalDateTime;
 import java.util.List;
 
-import static naeilmolae.domain.alarm.domain.AlarmCategory.*;
-
 @Service
 @RequiredArgsConstructor
 public class PushNotificationService {
@@ -25,8 +23,6 @@ public class PushNotificationService {
     private final NotificationContext notificationContext;
 
     @Transactional(readOnly = true)
-    // TODO 수정 필요함
-    //todo fcm 토큰 없는 경우 예외 처리 context에서 처리
     public void sendNotificationsAtScheduledTime() {
         List<Member> youthMembers = memberService.getAllYouthMemeber();
         LocalDateTime now = LocalDateTime.now();
