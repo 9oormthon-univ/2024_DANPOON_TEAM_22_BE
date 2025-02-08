7251766aa9d44ab262a713520620561ab5244b67
shimfff <shimseawon1510@gmail.com>
2025-02-08 14:19:42 +0900

    :bug: fix: id 필드 설정 수정

--- src/main/java/naeilmolae/domain/weather/domain/Grid.java
+++ src/main/java/naeilmolae/domain/weather/domain/Grid.java
@@ -2,6 +2,7 @@ package naeilmolae.domain.weather.domain;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -12,7 +13,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Grid {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private Integer x;
