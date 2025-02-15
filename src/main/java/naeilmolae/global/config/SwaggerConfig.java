10549c90c5fd0f230c676f5379b2b7aa1d19c376
useongee <118250509+USEONGEE@users.noreply.github.com>
2025-02-15 18:02:05 +0900

    Update SwaggerConfig.java

--- src/main/java/naeilmolae/global/config/SwaggerConfig.java
+++ src/main/java/naeilmolae/global/config/SwaggerConfig.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -27,7 +28,8 @@ public class SwaggerConfig {
         return new OpenAPI()
                 .addSecurityItem(securityRequirement)
                 .components(components)
-                .info(apiInfo());
+                .info(apiInfo())
+                .addServersItem(new Server().url("https://api.naeilmorae.co.kr").description("Production Server"));
     }
 
     private Info apiInfo() {
