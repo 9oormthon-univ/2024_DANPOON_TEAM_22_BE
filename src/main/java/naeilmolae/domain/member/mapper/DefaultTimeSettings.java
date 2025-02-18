85a16b3282f063bc67671fae6dedae5b4c53b488
useongee <118250509+USEONGEE@users.noreply.github.com>
2025-02-18 10:20:30 +0900

    fix: 시간 관련은 LocalTime으로 변경

--- src/main/java/naeilmolae/domain/member/mapper/DefaultTimeSettings.java
+++ src/main/java/naeilmolae/domain/member/mapper/DefaultTimeSettings.java
@@ -1,12 +1,13 @@
 package naeilmolae.domain.member.mapper;
 
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 public class DefaultTimeSettings {
 
-    public static final LocalDateTime DEFAULT_WAKE_UP_TIME = LocalDateTime.of(1970, 1, 1, 8, 0);
-    public static final LocalDateTime DEFAULT_SLEEP_TIME = LocalDateTime.of(1970, 1, 1, 22, 0);
-    public static final LocalDateTime DEFAULT_BREAKFAST_TIME = LocalDateTime.of(1970, 1, 1, 8, 30);
-    public static final LocalDateTime DEFAULT_LUNCH_TIME = LocalDateTime.of(1970, 1, 1, 12, 0);
-    public static final LocalDateTime DEFAULT_DINNER_TIME = LocalDateTime.of(1970, 1, 1, 19, 0);
+    public static final LocalTime DEFAULT_WAKE_UP_TIME = LocalTime.of(8, 0);
+    public static final LocalTime DEFAULT_SLEEP_TIME = LocalTime.of(22, 0);
+    public static final LocalTime DEFAULT_BREAKFAST_TIME = LocalTime.of(8, 30);
+    public static final LocalTime DEFAULT_LUNCH_TIME = LocalTime.of(12, 0);
+    public static final LocalTime DEFAULT_DINNER_TIME = LocalTime.of(19, 0);
 }
